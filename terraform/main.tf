07d67a90daa8089f09c601653c68b10724818750
WassimMensi <wassim.mensi@ecole-isitech.fr>
2025-04-24 12:19:12 +0200

    Add Windows AMD64 support for Docker Terraform provider v3.3.0
    - Added LICENSE file for the Mozilla Public License Version 2.0.
    - Created README.md with documentation and example usage for the Docker provider.
    - Included the compiled binary for the Docker provider (terraform-provider-docker_v3.3.0.exe).
    - Initialized main.tf with basic configuration for Docker provider and resources for nginx and MySQL containers.
    - Created terraform.tfstate to manage the state of the Docker resources.
    - Added a backup of the terraform.tfstate for safety.

---
+++ terraform/main.tf
@@ -0,0 +1,35 @@
+terraform {
+  required_providers {
+    docker = {
+      source = "kreuzwerker/docker"
+    }
+  }
+}
+
+provider "docker" {}
+
+resource "docker_image" "nginx" {
+  name = "nginx:latest"
+}
+
+resource "docker_container" "web" {
+  name  = "mars-rover-web"
+  image = docker_image.nginx.name
+  ports {
+    internal = 80
+    external = 8080
+  }
+}
+
+resource "docker_container" "db" {
+    name = "mars-db"
+    image = "mysql:8.0"
+    env = ["MYSQL_ROOT_PASSWORD=rov3r"]
+    networks_advanced {
+        name = docker_network.mars_net.name
+    }
+}
+
+resource "docker_network" "mars_net" {
+    name = "mars-net"
+}
